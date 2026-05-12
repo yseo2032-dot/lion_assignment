470d49076c0600f990194017d82ad9c4792aec1c
jungyunseo <yseo2032@gmail.com>
2026-05-13 04:05:35 +0900

    과제 추가

---
+++ 0512/0512/src/App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from "react";
+import "./App.css";
+import Login from "./login";
+
+function App() {
+  return (
+    <div className="container">
+      <Login />
+    </div>
+  ); 
+}
+
+export default App;
